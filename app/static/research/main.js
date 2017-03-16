352f09cd7d8e0dc82b1429f2d1541d9dd052ba78
Likit Preeyanon <likit.pre@mahidol.edu>
2017-03-16 23:09:10 +0700

    added more sort funtionality
    added a modal for abstract text

--- app/static/research/main.js
+++ app/static/research/main.js
@@ -6,7 +6,9 @@ var viewModel = function() {
     var self = this;
     self.articles = ko.observableArray([]);
     self.filteredArticles = ko.observableArray([]);
-    self.reverse_sort = ko.observable(false);
+    self.reverse_sort_date = ko.observable(false);
+    self.reverse_sort_title = ko.observable(false);
+    self.reverse_sort_journal = ko.observable(false);
     self.query = ko.observable('');
     self.search = function (query) {
         self.filteredArticles([]);
@@ -16,7 +18,7 @@ var viewModel = function() {
             $.each(self.articles(), function(idx, ab) {
                 if(ab.title.toLowerCase().indexOf(query.toLowerCase()) > -1 ||
                     ab.journal.toLowerCase().indexOf(query.toLowerCase()) > -1 ||
-                    ab.authors.toLowerCase().indexOf(query.toLowerCase()) > -1) {
+                    ab.authorList.toLowerCase().indexOf(query.toLowerCase()) > -1) {
                     self.filteredArticles.push(ab);
                 }
             });
@@ -29,12 +31,19 @@ $.getJSON("http://localhost:5050/api/abstracts/2017", function(data) {
         var auths = [];
         var date = new Date(d.cover_date);
         date = date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
+        var authorList = [];
+        $.each(d.authors, function(_, auth) {
+            authorList.push(auth.name);
+        })
         var abs = {
             title: d.title,
             journal: d.journal,
-            cover_date: new Date(d.cover_date),
+            coverDate: new Date(d.cover_date),
             authors: d.authors,
-            cover_date_string: date
+            authorList: authorList.join(', '),
+            coverDateString: date,
+            citedByCount: d.citedby_count,
+            text: d.abstract,
         };
         vm.articles.push(abs);
     });
@@ -43,13 +52,34 @@ $.getJSON("http://localhost:5050/api/abstracts/2017", function(data) {
 ko.applyBindings(vm);
 vm.query.subscribe(vm.search);
 function sortArticlesByDate() {
-    vm.articles.sort(function(a,b) {
-        if(vm.reverse_sort()===false) {
-            return a.cover_date < b.cover_date;
+    vm.filteredArticles.sort(function(a,b) {
+        if(vm.reverse_sort_date()===false) {
+            return a.coverDate < b.coverDate;
         } else {
-            return a.cover_date > b.cover_date;
+            return a.coverDate > b.coverDate;
         }
     });
-    vm.reverse_sort(!vm.reverse_sort()); // toggle sort order
-    console.log(vm.reverse_sort());
+    vm.reverse_sort_date(!vm.reverse_sort_date()); // toggle sort order
+};
+
+function sortArticlesByTitle() {
+    vm.filteredArticles.sort(function(a,b) {
+        if(vm.reverse_sort_title()===false) {
+            return a.title < b.title;
+        } else {
+            return a.title > b.title;
+        }
+    });
+    vm.reverse_sort_title(!vm.reverse_sort_title()); // toggle sort order
+};
+
+function sortArticlesByJournal() {
+    vm.filteredArticles.sort(function(a,b) {
+        if(vm.reverse_sort_journal()===false) {
+            return a.journal < b.journal;
+        } else {
+            return a.journal > b.journal;
+        }
+    });
+    vm.reverse_sort_journal(!vm.reverse_sort_journal()); // toggle sort order
 };
