0683c65a74f9357143eea99a463498a0a7be99aa
Likit Preeyanon <likit.pre@mahidol.edu>
2017-05-18 14:55:29 +0700

    added main employees page with some pie charts

--- app/employee/views.py
+++ app/employee/views.py
@@ -2,6 +2,11 @@ from flask import render_template
 from . import employee_blueprint as employee_bp
 
 
+@employee_bp.route('/')
+def main():
+    return render_template('employee/main.html')
+
+
 @employee_bp.route('/department/<department_slug>')
 def list_staff(department_slug):
     return render_template('employee/department.html', dept_slug=department_slug)
