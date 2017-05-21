aa5f7107be9331e6b5b01f8087be75e1a67dccdc
Likit Preeyanon <likit.pre@mahidol.edu>
2017-05-22 01:28:29 +0700

    added license exam
    added wrs

--- app/static/education/main.js
+++ app/static/education/main.js
@@ -1,8 +1,11 @@
 var ctxWRS = document.getElementById('wrs-chart').getContext('2d');
 var ctxSatisfaction = document.getElementById('satisfaction-chart').getContext('2d');
+var ctxMtLicense = document.getElementById('mt-license-chart').getContext('2d');
+var ctxRtLicense = document.getElementById('rt-license-chart').getContext('2d');
 
 var evals = $.getJSON("http://localhost/api/education/evaluation/edpex/wrs/");
 var satis = $.getJSON("http://localhost/api/education/evaluation/edpex/satisfaction/")
+var license = $.getJSON("http://localhost/api/education/evaluation/edpex/license/")
 
 $.when(evals).done(function(data) {
     var scoreData = {};
@@ -109,6 +112,142 @@ $.when(satis).done(function(data) {
             },
             legend: {
                 display: false
+            }
+        }
+    })
+})
+
+$.when(license).done(function(data) {
+    var years = [];
+    var mumt = [];
+    var mtkku = [];
+    var mtcmu = [];
+    var murt = [];
+    var rtcmu = [];
+    $.each(data.data.mt, function(idx, d) {
+        if(d.institute==="MUMT") {
+            years.push(d.year);
+            mumt.push(d.percent);
+        } else if (d.institute==="MT-KKU") {
+            mtkku.push(d.percent);
+        } else if (d.institute==="MT-CMU") {
+            mtcmu.push(d.percent);
+        } else {
+            // pass
+        }
+    })
+    $.each(data.data.rt, function(idx, d) {
+        if(d.institute==="MURT") {
+            murt.push(d.percent);
+        } else if (d.institute==="RT-CMU") {
+            rtcmu.push(d.percent);
+        } else {
+            // pass
+        }
+    })
+    var mtLicenseChart = new Chart(ctxMtLicense, {
+        type: 'line',
+        data: {
+            labels: years,
+            datasets: [
+                {
+                    data: mumt,
+                    label: "MUMT",
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "#0317AF",
+                    borderColor: "#0317AF"
+                },
+                {
+                    data: mtcmu,
+                    label: "MT-CMU",
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "#9803AF",
+                    borderColor: "#9803AF"
+                },
+                {
+                    data: mtkku,
+                    label: "MT-KKU",
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "#AF7303",
+                    borderColor: "#AF7303"
+                }
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        fontSize: 14
+                    },
+                    scaleLabel: {
+                        labelString: "ร้อยละผู้สอบผ่าน",
+                        fontSize: 16,
+                        display: true
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        fontSize: 14
+                    }
+                }]
+            },
+            legend: {
+                labels: {
+                    fontSize: 14
+                }
+            }
+        }
+    })
+    var rtLicenseChart = new Chart(ctxRtLicense, {
+        type: 'line',
+        data: {
+            labels: years,
+            datasets: [
+                {
+                    data: murt,
+                    label: "MURT",
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "#0317AF",
+                    borderColor: "#0317AF"
+                },
+                {
+                    data: rtcmu,
+                    label: "RT-CMU",
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "#9803AF",
+                    borderColor: "#9803AF"
+                },
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        fontSize: 14
+                    },
+                    scaleLabel: {
+                        labelString: "ร้อยละผู้สอบผ่าน",
+                        fontSize: 16,
+                        display: true
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        fontSize: 14
+                    }
+                }]
+            },
+            legend: {
+                labels: {
+                    fontSize: 14
+                }
             }
         }
     })
