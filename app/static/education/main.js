94309a67551526c0d67ec389cc6f18c43d72bfce
Likit Preeyanon <likit.pre@mahidol.edu>
2017-05-21 21:34:25 +0700

    added link to education page in main page
    added WRS evaluation chart

--- app/static/education/main.js
+++ app/static/education/main.js
@@ -1,22 +1,62 @@
-var ctx = document.getElementById('wrs-chart').getContext('2d');
-var data = {
-    labels: ["red", "blue", "yellow"],
-    datasets: [{
-        data: [300, 50, 100],
-        backgroundColor: [
-            "#FF6384",
-            "#36A2EB",
-            "#FFCE56"
-        ],
-        hoverBackgroundColor: [
-            "#FF6384",
-            "#36A2EB",
-            "#FFCE56"
-        ]
-    }]
-};
+var ctxWRS = document.getElementById('wrs-chart').getContext('2d');
 
-var chart = new Chart(ctx, {
-    type: 'pie',
-    data: data,
+var evals = $.getJSON("http://localhost/api/education/evaluation/edpex/wrs/");
+
+$.when(evals).done(function(data) {
+    var scoreData = {};
+    var slugs = [];
+    var descs = [];
+
+    $.each(data, function(idx, d) {
+        slugs.push(d.slug);
+        descs.push(d.desc)
+        var dat = [];
+        $.each(d.scores, function(i, s) {
+            dat.push(s.score);
+        })
+        scoreData[d.slug] = dat;
+    });
+
+    var years = [];
+    for(var i=0; i<data[0].scores.length; i++) {
+        years.push(data[0].scores[i].year);
+    }
+
+    var plotData = [];
+    var backgroundColor = [
+        "#FF6384",
+        "#36A2EB",
+        "#00CE56",
+        "#555E56",
+        "#FFCE56"
+    ]
+    $.each(slugs, function(ix, s) {
+        plotData.push({
+            data: scoreData[s],
+            label: descs[ix],
+            backgroundColor: backgroundColor[ix],
+        })
+    })
+    console.log(plotData);
+    plotBarChart(ctxWRS, plotData, years);
 });
+
+var plotBarChart = function(ctx, data, years) {
+    var plotData = {
+        labels: years,
+        datasets: data,
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                    }
+                }]
+            }
+        }
+    };
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: plotData,
+    });
+}
